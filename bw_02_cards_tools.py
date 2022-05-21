7af735d6435fbe6f6d5709f57bff33690d09b927
benslwang <363111516@qq.com>
2022-05-21 17:23:18 +0800

    P303-P321

--- bw_02_cards_tools.py
+++ bw_02_cards_tools.py
@@ -1,3 +1,6 @@
+# 用列表记录所有的名片字典
+card_list = []
+
 def show_menu():
 
     """菜单显示"""
@@ -18,12 +21,54 @@ def new_card():
     print("-" * 50)
     print("新增名片")
 
+    # 1.提示用户输入名片的详细信息
+    name_str = input("请输入姓名：")
+    phone_str = input("请输入电话：")
+    qq_str = input("请输入QQ号码：")
+    email_str = input("请输入邮箱地址：")
+
+    # 2.使用用户输入的名片信息建立一个字典
+    card_dict = {"name": name_str,
+                 "phone": phone_str,
+                 "qq": qq_str,
+                 "email": email_str}
+    # 3.将名片字典添加到列表中
+    card_list.append(card_dict)
+    # print(card_list)
+    # 4.提示用户添加成功
+    print("%s 的名片已添加成功！" % name_str)
 
 def show_all():
 
     """显示所有名片"""
     print("-" * 50)
-    print("现实所有名片")
+    print("显示所有名片")
+
+    # 判断是否有名片记录，如没有，提示用户并且返回
+    if len(card_list) == 0:
+        print("当前没有任何的名片记录，请使用新增功能添加名片！")
+        # return可以返回一个函数的执行结果
+        # 下方的代码不会被执行
+        # 如果return后面没有任何的内容，表示会返回到调用函数的位置
+        # 并且不返回任何的结果
+        return
+
+    # 打印表头(遍历列表，以不换行的形式输出，作为表头)
+    for title in ["姓名", "电话", "qq", "邮箱"]:
+        print(title, end="\t\t")
+
+    print("")
+
+    # 打印分割线
+    print("=" * 50)
+    # 遍历名片列表依次输出字典信息
+    for card_dict in card_list:
+        print("%s\t\t%s\t\t%s\t\t%s" % (card_dict["name"],
+              card_dict["phone"],
+              card_dict["qq"],
+              card_dict["email"]))
+    # 打印分割线
+    print("=" * 50)
 
 
 def search_card():
@@ -31,3 +76,40 @@ def search_card():
     """搜索名片"""
     print("-" * 50)
     print("搜索名片")
+
+    # 1.提示用户输入要搜索的姓名
+    search_info = input("请输入您要查询的信息：")
+
+    # 2.遍历名片列表，查询要搜索的姓名，如果没有找到，需要提示用户
+    for card_dict in card_list:
+        if search_info == card_dict["name"] \
+                or search_info == card_dict["phone"] \
+                or search_info == card_dict["qq"] \
+                or search_info == card_dict["email"]:
+            print("姓名\t\t电话\t\tqq\t\t邮箱")
+            print("=" * 50)
+            print("%s\t\t%s\t\t%s\t\t%s" % (card_dict["name"],
+                                            card_dict["phone"],
+                                            card_dict["qq"],
+                                            card_dict["email"]))
+            # todo 针对找到的名片记录执行修改和删除的操作
+            deal_card(card_dict)
+
+            break
+    else:
+        print("抱歉，未查询到%s相关的名片信息，请重新输入！" % search_info)
+
+
+def deal_card(find_dict):
+    """# todo 定义"""
+    print(find_dict)
+    action_str = input("请选择要执行的操作："
+                       "[1] 修改 [2] 删除 [0] 返回上级菜单 ")
+    if action_str == "1":
+        print("修改名片")
+
+    elif action_str == "2":
+        print("删除名片")
+    else:
+        print("输入错误，请重新查询！")
+
